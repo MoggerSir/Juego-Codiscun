ecd1706ceb19cbb3baec6d0b4b9ebe7e688e1934
Enrique Garcia Santiago <garciakike06@gmail.com>
2026-04-19 20:41:15 -0500

    Revert "prueba 1"
    This reverts commit b0781b4492ef71ae34d924ee3ed4ff0c05d498d6.

--- public/assets/tilemaps/background.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="background" tilewidth="32" tileheight="32" tilecount="980" columns="49">
- <image source="../sprites/objetos/background.png" width="1568" height="640"/>
-</tileset>
--- public/assets/tilemaps/tileset_64x64(new).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="tileset_64x64(new)" tilewidth="32" tileheight="32" tilecount="288" columns="24">
- <image source="../sprites/objetos/tileset_64x64(new).png" width="768" height="384"/>
-</tileset>
